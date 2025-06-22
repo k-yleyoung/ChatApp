012125e91c03961e4980c463e8c31d9cf81072b9
k-yleyoung <144155897+k-yleyoung@users.noreply.github.com>
2025-06-22 17:50:40 -0500

    1:03 added login/logout routes

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,6 +6,12 @@ export const generateToken= (userId, res) => {
     })
 
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 1000 //7 days in millseconds
+        maxAge: 7 * 24 * 60 * 1000, //7 days in millseconds
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV !== "development"
+
     })
+
+    return token;
 }
